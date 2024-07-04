49920bfaf8b17963f7f3c232645de82493512363
Nic01asFr <laval.nicolas13@gmail.com>
2024-07-05 00:09:13 +0200

    Modify cloud apparition position

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -155,9 +155,9 @@ function Skills() {
         cloud.group.position.x += cloud.speed * (1 - (1 - cloud.depth*1.2));
 
         // Check if the right edge of the cloud has reached the right edge of the canvas
-        if (cloud.group.position.x > cloud.width * 8) {
+        if (cloud.group.position.x > cloud.width * 9) {
           // Reposition the cloud to the left
-          cloud.group.position.x = -cloud.width * 8;
+          cloud.group.position.x = -cloud.width * 9;
           cloud.group.position.y = Math.random() * 30 - 15;
         }
 
